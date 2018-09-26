b0fc7fdeefc8ce9bc6f711d59b84a2ea9f05d157
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2018-09-26 15:11:59 +0700

    first commit

---
+++ src/main/java/com/github/ipan97/learnspringaop/LearnSpringAopApplication.java
@@ -0,0 +1,12 @@
+package com.github.ipan97.learnspringaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringAopApplication.class, args);
+	}
+}
